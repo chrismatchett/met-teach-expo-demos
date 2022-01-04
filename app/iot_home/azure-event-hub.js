a547d4aa7dbd15098cbf18792e0c9bea01d1526b
Chris <chris@freeperiod.co.uk>
2022-01-04 19:42:20 +0000

    mqtt

--- app/iot_home/azure-event-hub.js
+++ app/iot_home/azure-event-hub.js
@@ -13,56 +13,10 @@ import {
   NativeBaseProvider,
 } from "native-base"
 
-import { Client, Message } from 'react-native-paho-mqtt';
-
 const HomeScreen = ({ route, navigation }) => {
   const [data, setData] = React.useState(null); 
 
-  //Set up an in-memory alternative to global localStorage
-  // const myStorage = {
-  //   setItem: (key, item) => {
-  //     myStorage[key] = item;
-  //   },
-  //   getItem: (key) => myStorage[key],
-  //   removeItem: (key) => {
-  //     delete myStorage[key];
-  //   },
-  // };
-  
-  // Create a client instance
-  const client = new Client({ uri: 'ws://192.168.28.152:9001'});
-  
-  // set event handlers
-  client.on('connectionLost', (responseObject) => {
-    if (responseObject.errorCode !== 0) {
-      setData(responseObject.errorMessage);
-    }
-  });
-  client.on('messageReceived', (message) => {
-    setData(message.payloadString);
-  });
-  
-  // connect the client
-  client.connect()
-    .then(() => {
-      // Once a connection has been made, make a subscription and send a message.
-      //console.log('onConnect');
-      //return client.subscribe('World');
-      return client.publish('zigbee2mqtt/0x001788010b2efdb0/set', '{"STATE": "ON"}');
-    })
-    .then(() => {
-      //const message = new Message('Hello');
-      //message.destinationName = 'World';
-      //client.send(message);
-    })
-    .catch((responseObject) => {
-      if (responseObject.errorCode !== 0) {
-        setData('onConnectionLost:' + responseObject.errorMessage);
-      }
-    })
-  ;
-
-/*   const turnOn = () => {
+  const turnOn = () => {
 
     let myHeaders = new Headers();
     myHeaders.append("Authorization", "SharedAccessSignature sr=met-events.servicebus.windows.net&sig=Yuso42BOpx8hTF8iA9doxSwmc6wppl%2FivOTqdcqnaLk%3D&se=1638980611&skn=send");
@@ -88,7 +42,7 @@ const HomeScreen = ({ route, navigation }) => {
 
   const turnOff = () => {
     Alert.alert("Turn Off", "Off");
-  }; */
+  };
     
   return (
     <View>
