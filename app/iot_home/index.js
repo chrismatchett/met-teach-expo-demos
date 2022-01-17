7dbbd0c8202af0d37c0061bac1560793a12368eb
Chris <chris@freeperiod.co.uk>
2022-01-17 16:32:04 +0000

    mqtt success

--- app/iot_home/index.js
+++ app/iot_home/index.js
@@ -16,86 +16,62 @@ import {
 import { Client, Message } from 'react-native-paho-mqtt';
 
 const HomeScreen = ({ route, navigation }) => {
-  const [data, setData] = React.useState(null); 
+const [data, setData] = React.useState(null); 
+//Set up an in-memory alternative to global localStorage
+const myStorage = {
+  setItem: (key, item) => {
+    myStorage[key] = item;
+  },
+  getItem: (key) => myStorage[key],
+  removeItem: (key) => {
+    delete myStorage[key];
+  },
+};
 
-  //Set up an in-memory alternative to global localStorage
-  // const myStorage = {
-  //   setItem: (key, item) => {
-  //     myStorage[key] = item;
-  //   },
-  //   getItem: (key) => myStorage[key],
-  //   removeItem: (key) => {
-  //     delete myStorage[key];
-  //   },
-  // };
-  
-  // Create a client instance
-  const client = new Client({ uri: 'ws://192.168.28.152:9001'});
-  
-  // set event handlers
-  client.on('connectionLost', (responseObject) => {
+// Create a client instance
+const client = new Client({ uri: 'ws://192.168.0.221:9001/', clientId: 'clientId', storage: myStorage });
+
+// set event handlers
+client.on('connectionLost', (responseObject) => {
+  if (responseObject.errorCode !== 0) {
+    console.log(responseObject.errorMessage);
+  }
+});
+client.on('messageReceived', (message) => {
+  console.log(message.payloadString);
+});
+
+// connect the client
+client.connect()
+  .then(() => {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log('onConnect');
+    return client.subscribe('zigbee2mqtt/bridge/devices');
+    //return client.publish('zigbee2mqtt/0x001788010b2efdb0/set', {"state": "off"})
+
+  })
+  .then(() => {
+    const message = new Message("on");
+    message.destinationName = 'zigbee2mqtt/0x001788010b2efdb0/set';
+    client.send(message);
+  })
+  .catch((responseObject) => {
+    console.log(responseObject)
     if (responseObject.errorCode !== 0) {
-      setData(responseObject.errorMessage);
+      console.log('onConnectionLost:' + responseObject.errorMessage);
     }
-  });
-  client.on('messageReceived', (message) => {
-    setData(message.payloadString);
-  });
-  
-  // connect the client
-  client.connect()
-    .then(() => {
-      // Once a connection has been made, make a subscription and send a message.
-      //console.log('onConnect');
-      //return client.subscribe('World');
-      return client.publish('zigbee2mqtt/0x001788010b2efdb0/set', '{"STATE": "ON"}');
-    })
-    .then(() => {
-      //const message = new Message('Hello');
-      //message.destinationName = 'World';
-      //client.send(message);
-    })
-    .catch((responseObject) => {
-      if (responseObject.errorCode !== 0) {
-        setData('onConnectionLost:' + responseObject.errorMessage);
-      }
-    })
-  ;
+  })
+;
 
-/*   const turnOn = () => {
 
-    let myHeaders = new Headers();
-    myHeaders.append("Authorization", "SharedAccessSignature sr=met-events.servicebus.windows.net&sig=Yuso42BOpx8hTF8iA9doxSwmc6wppl%2FivOTqdcqnaLk%3D&se=1638980611&skn=send");
-    myHeaders.append("Content-Type", "application/atom+xml;type=entry;charset=utf-8");
-    myHeaders.append("Host", "met-events.servicebus.windows.net");
-    
-    let raw = "{\"device-id\": \"1355\", \"action\": \"stop\"}";
-    
-    let requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("https://met-events.servicebus.windows.net/nightclass/messages", requestOptions)
-      //.then(response => response.text())
-      .then(response => response.status)
-      //.then(result => console.log(result))
-      .then(result => setData(result))
-      .catch(error =>  console.log('error', error));
-  };
 
-  const turnOff = () => {
-    Alert.alert("Turn Off", "Off");
-  }; */
-    
+
   return (
     <View>
-      <Button onPress={turnOn}>
+      <Button>
         <Text>Turn On</Text>
       </Button>
-      <Button onPress={turnOff}>
+      <Button>
         <Text>Turn Off</Text>
       </Button>
       <Text>{data}</Text>
